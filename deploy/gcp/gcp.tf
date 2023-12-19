2f10d4ee32c2a393da4dde01aabfb80582486ef0
Stepan Fedorov <StFedorov@gmail.com>
2023-12-18 19:11:13 -0600

    New: deploy/gcp/

---
+++ deploy/gcp/gcp.tf
@@ -0,0 +1,24 @@
+provider "google" {
+  project = "common-shit"
+  region  = "us-west1"
+}
+
+locals {
+  services = [
+    "iam.googleapis.com",
+    "cloudfunctions.googleapis.com"
+  ]
+}
+
+resource "google_project_service" "project" {
+  count   = length(local.services)
+  service = local.services[count.index]
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_dependent_services = true
+  disable_on_destroy         = true
+}
